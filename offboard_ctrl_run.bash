7ec63c240880b722d5d049bc1d3c0e4348669570
ceu <cego6160@colorado.edu>
2023-10-28 17:30:03 -0600

    changed dev scripts to rplidar standard

--- offboard_ctrl_run.bash
+++ offboard_ctrl_run.bash
@@ -6,6 +6,6 @@
 source /opt/ros/foxy/setup.bash #source environment, ros2
 source $PWD/install/local_setup.bash #source local workspace, custom
 #launch sitl in new window 
-gnome-terminal -- /bin/sh -c 'cd $PWD/src/PX4-Autopilot/;  make px4_sitl gazebo-classic_iris' 
+gnome-terminal -- /bin/sh -c 'cd $PWD/src/PX4-Autopilot/;  make px4_sitl gazebo-classic_iris_rplidar' 
 #launch middleware & listener simultaneously [change listener to custom action TBD]
 (trap 'kill 0' SIGINT; MicroXRCEAgent udp4 -p 8888 & ros2 launch px4_offboard offboard_position_control.launch.py) 
